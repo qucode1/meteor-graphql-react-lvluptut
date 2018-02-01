8f1500a2519fc1a55de208a442e99640fc63acfd
Yannick Panis <info@qucode.eu>
2018-02-01 07:42:52 +0100

    add resolutions query to App.js & render resolutions

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { Fragment } from "react"
 import gql from "graphql-tag"
 import { graphql } from "react-apollo"
 
-const App = props => <h1>{props.data.hi}</h1>
+const App = props =>
+  props.data.loading ? null : (
+    <Fragment>
+      <h1>{props.data.hi}</h1>
+      <ul>
+        {props.data.resolutions.map(resolution => (
+          <li key={resolution._id}>{resolution.name}</li>
+        ))}
+      </ul>
+    </Fragment>
+  )
 
 const hiQuery = gql`
   {
     hi
+    resolutions {
+      _id
+      name
+    }
   }
 `
 
